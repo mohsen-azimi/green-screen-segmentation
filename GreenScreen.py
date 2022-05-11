7a655cafa99ef1d318da0c8d260ba077afddeb79
Mohsen <azimi.eng@gmail.com>
2022-05-10 18:48:34 -0700

    updated

--- GreenScreen.py
+++ GreenScreen.py
@@ -3,18 +3,35 @@ import numpy as np
 import glob
 import os
 
+# Label ids of the dataset (BGR)
+mask_colors = {
+    "circular": (255, 0, 0),
+    "rectangular": (255, 0, 0),
+    "girder": (255, 255, 0),
+}
+''' "(0, 0, 0)": 0,  # outlier
+    "(255, 0, 0)": 1,  # circular
+    "(255, 255, 0)": 2,  # rectangular
+    "(128, 0, 255)": 3,  # girder
+    "(255, 128, 0)": 4,  # ...
+    "(0, 0, 255)": 5,  # ...
+    "(128, 255, 255)": 6,  # ...
+    "(0, 255, 0)": 7,  # ...
+    "(128, 128, 128)": 8  # ...'''
+
+object_color = mask_colors["rectangular"]
 
 video = cv2.VideoCapture(1)
-background = cv2.imread("rgb.png")
+
+lab_background = cv2.imread("rgb.png")
 
 
 def nothing(x):
     pass
 
 
-# Create a black image, a window
-img = np.zeros((300, 512, 3), np.uint8)
 cv2.namedWindow('control')
+cv2.resizeWindow('control', 500, 500)
 
 w, h = 640, 480
 cv2.createTrackbar("LH", "control", 0, 255, nothing)
@@ -25,17 +42,17 @@ cv2.createTrackbar("US", "control", 255, 255, nothing)
 cv2.createTrackbar("UV", "control", 40, 255, nothing)
 
 cv2.createTrackbar("crop_T", "control", 173, int(h / 2), nothing)
-cv2.createTrackbar("crop_B", "control", 95, int(h / 2), nothing)
+cv2.createTrackbar("crop_B", "control", 30, int(h / 2), nothing)
 cv2.createTrackbar("crop_L", "control", 155, int(w / 2), nothing)
 cv2.createTrackbar("crop_R", "control", 190, int(w / 2), nothing)
 
 cv2.createTrackbar('save', 'control', 0, 1, nothing)
 
 # Get the latest generated file from the output directory
-if len(os.listdir('output/image')) == 0:
+if len(os.listdir('output/output')) == 0:
     i = 0
 else:
-    list_of_created = glob.glob('output/image/*') # * means all if need specific format then *.csv
+    list_of_created = glob.glob('output/image/*')  # * means all if need specific format then *.csv
     latest_file = max(list_of_created, key=os.path.getctime)
     i = int(latest_file.split()[1][:-4])
 
@@ -44,7 +61,7 @@ while True:
     ret, input_frame = video.read()
 
     input_frame = cv2.resize(input_frame, (w, h))
-    background = cv2.resize(background, (w, h))
+    lab_background = cv2.resize(lab_background, (w, h))
 
     l_green = np.array([cv2.getTrackbarPos("LH", "control"),
                         cv2.getTrackbarPos("LS", "control"),
@@ -71,22 +88,24 @@ while True:
     kernel = np.ones((3, 3), 'uint8')
     # mask = cv2.dilate(mask, kernel, iterations=1)
     mask = cv2.erode(mask, kernel, iterations=1)
+    mask_colored = np.zeros([h, w, 3], dtype=np.uint8)
+    mask_colored[:, :] = [object_color[0], object_color[1], object_color[2]]
 
-    res = cv2.bitwise_and(input_frame, input_frame, mask=cv2.bitwise_not(mask)) # invert back the mask
+    res = cv2.bitwise_and(input_frame, input_frame, mask=cv2.bitwise_not(mask))  # invert back the mask
     image = input_frame - res
-    image = np.where(image == 0, background, image)
+    image = np.where(image == 0, lab_background, image)
+    mask2 = np.where((input_frame - res) == 0, np.zeros([h, w, 3], dtype=np.uint8), mask_colored)
 
-    cv2.imshow("input_frame", input_frame)
-    cv2.imshow("mask", mask)
-    cv2.imshow("image", image)
+    cv2.imshow("input_frame", np.vstack((input_frame, image, mask2)) )
 
     s = cv2.getTrackbarPos('save', 'control')
 
     if s == 1:
         print(f"Saving the image {i} ...")
         cv2.imwrite(f'output/input/frame {i}.png', input_frame)
-        cv2.imwrite(f'output/mask/frame {i}.png', mask)
-        cv2.imwrite(f'output/image/frame {i}.png', image)
+        cv2.imwrite(f'output/binary_mask/frame {i}.png', mask)
+        cv2.imwrite(f'output/color_mask/frame {i}.png', mask2)
+        cv2.imwrite(f'output/output/frame {i}.png', image)
         # cv2.imwrite('output/mask.png', mask)
         i += 1
         s = cv2.setTrackbarPos('save', 'control', 0)
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## Collect Segmentation Data Using Green Screen
-These simple codes use OpenCV tools to create segmentation (ground truth) dataset.
+These simple codes use OpenCV tools to create segmentation (ground truth) dataset. Press `ESC` to stop the code.
+
+![Control](R:\GitHub\Green-Screen-Segmentation\src\control.JPG)
+
+Input, output, and color-coded segmentation mask
+
+![Output](R:\GitHub\Green-Screen-Segmentation\src\outputs.JPG)
 
